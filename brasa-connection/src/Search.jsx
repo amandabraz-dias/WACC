c1a808598f9d369710c4612040156cedb1a006af
valentinaagarcia <71085526+valentinaagarcia@users.noreply.github.com>
2024-03-09 14:06:39 -0500

    Routing

---
+++ brasa-connection/src/Search.jsx
@@ -0,0 +1,13 @@
+function Search () {
+    return (
+    <body class="background-overlay">
+      <div id="search">
+        <img src="images/logoPlusName.png" alt="Logo" id="main-logo"/>
+        <br />
+        <input id="search-bar" placeholder="Search course or internship..." />
+      </div>
+      </body>
+    );
+}
+
+export default Search;
