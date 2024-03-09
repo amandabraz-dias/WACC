19e7276888a2d25fee3c66b2820304ae231bf328
Victoria Chang <victoriachang@Victorias-MacBook-Pro-2.local>
2024-03-09 13:52:26 -0500

    Created app file and profile page file - initial version

--- brasa-connection/src/App.jsx
+++ brasa-connection/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <body class="background-overlay">
+      <div id="search">
+        <img src="images/logoPlusName.png" alt="Logo" id="main-logo"/>
+        <br />
+        <input id="search-bar" placeholder="Search course or internship..." />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </body>
     </>
   )
 }
 
+
 export default App
+
+
--- brasa-connection/src/main.jsx
+++ brasa-connection/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Profilepage from './profilepage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profilepage />
   </React.StrictMode>,
 )
